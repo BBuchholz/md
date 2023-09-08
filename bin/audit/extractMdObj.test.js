9b44fa92a81038402f6e672a30adda03b229dd84
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-07 19:33:34 -0500

    Add unrecognized to test

--- bin/audit/extractMdObj.test.js
+++ bin/audit/extractMdObj.test.js
@@ -6,4 +6,6 @@ test('generates audit report from test file', async () => {
     expect(mdObj.contents[0]).toContain('A test markdown file')
     const mdObj2 = extractMdObj('testWithFM.md')
     expect(mdObj2.fm['uuid']).toBe('NOTSET')
+    const mdObj3 = extractMdObj('testWithUnRecFM.md')
+    expect(mdObj3.issues[0]).toContain('unrecognized')
 })
---
+++ testWithUnRecFM.md
@@ -0,0 +1,9 @@
+
+---
+
+uuid: NOTSET
+unrecognized: field
+
+---
+
+A test markdown file (without any markdown)
